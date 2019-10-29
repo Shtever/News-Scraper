e416028029d09ebb7f4b9805fc5eccf76570ed43
Stephen McMillen <Shtevetm@gmail.com>
2019-10-28 19:02:37 -0500

    index.html built.

--- server.js
+++ server.js
@@ -14,10 +14,38 @@ var app = express();
 
 // Boilerplate stuff for json
 app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Default folder
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/News-Scraper", {useNewUrlParser: true});
+// Mongo DB 
+mongoose.connect("mongodb://localhost/News-Scraper", { useNewUrlParser: true });
+
+// scrape route //
+app.get("/", function (req, res) {
+    axios.get("https://www.aljazeera.com/").then(function (response) {
+        var $ = cheerio.load(response.data);
+        var results = [];
+        $("h1.mts-article-title").each(function (i, element) {
+            var title = $(element).children().text();
+            var link = $(element).find("a").attr("href");
+            results.push({
+                title: title,
+                link: link
+            });
+        });
+
+        // Log the results once you've looped through each of the elements found with cheerio
+        console.log(results);
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
+});
+
+app.listen(PORT, function () {
+    console.log("App running: https://localhost:" + PORT);
+  });
+  
